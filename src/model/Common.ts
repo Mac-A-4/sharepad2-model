1c998177434bbbda80a21ac62d5a0f6adc2a382d
Mac-A-4 <malcolm.alvarez9898@gmail.com>
2023-01-21 22:51:34 -0800

    Fix again

--- src/model/Common.ts
+++ src/model/Common.ts
@@ -6,10 +6,10 @@ export const UserIDSchema = z.string().regex(/^[0-9]{6}$/)
 
 export const DocumentIDSchema = z.string().regex(/^[0-9]{6}$/)
 
-export const UserNameSchema = z.string().regex(/^\w{1,20}$/)
+export const UserNameSchema = z.string().regex(/^.{1,20}$/)
 
 export const UserTokenSchema = z.string().regex(/^[0-9]{6}/)
 
-export const DocumentNameSchema = z.string().regex(/^\w{1,100}$/)
+export const DocumentNameSchema = z.string().regex(/^.{1,100}$/)
 
-export const DocumentDataSchema = z.string().regex(/^.{0,1000000}$/)
+export const DocumentDataSchema = z.string().max(1000000)
